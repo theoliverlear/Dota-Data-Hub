5f8b67824f7b37c56e375cd7e509f44bb39f7da7
theoliverlear <sigwartholiver2@gmail.com>
2023-07-25 14:16:30 -0500

    Refactor to have constructor use specified methods

--- DotaDataHub/src/main/java/org/d2database/V2/Player/CoreDatabase.java
+++ DotaDataHub/src/main/java/org/d2database/V2/Player/CoreDatabase.java
@@ -1,5 +1,7 @@
 package org.d2database.V2.Player;
 
+import org.d2database.V2.Data;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.Connection;
@@ -12,12 +14,26 @@ public class CoreDatabase {
     Connection connection;
     String connectionUrl;
     public CoreDatabase() {
+        //-----------------------Load-Driver-And-Login------------------------
+        this.loadDriver();
+        this.setLoginData();
+        //-------------------------Create-Connection--------------------------
+        this.formConnectionUrl();
+        this.connect();
+    }
+    //------------------------------Load-Driver-------------------------------
+    public void loadDriver() {
+
         String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
         try {
             Class.forName(driver);
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
+
+    }
+    //----------------------------Set-Login-Data------------------------------
+    public void setLoginData() {
         String path = "C:\\Users\\olive\\OneDrive\\Documents\\" +
                 "Key Folder\\CryptoTraderLogin.txt";
         File file = new File(path);
@@ -35,15 +51,25 @@ public class CoreDatabase {
                 password = scanner.next();
             }
         }
+        Data.USERNAME.setData(username);
+        Data.PASSWORD.setData(password);
+    }
+    //--------------------------Form-Connection-URL---------------------------
+    public void formConnectionUrl() {
         String serverPrefix = "jdbc:sqlserver://";
         String server = "dota-data-hub.database.windows.net:1433;";
         String database = "database=DotaDataHubDatabase;";
-        String user = "user={" + username + "};password={" + password + "};";
-        String ending = "encrypt=true;trustServerCertificate=false;" +
+        String user = "user={" + Data.USERNAME.getData() + "};password={" +
+                                 Data.PASSWORD.getData() + "};";
+        String connProperties = "encrypt=true;trustServerCertificate=false;" +
                 "hostNameInCertificate=*.database.windows.net;" +
                 "loginTimeout=30;authentication=ActiveDirectoryPassword" +
                 ";autoReconnect=true";
-        this.connectionUrl = serverPrefix + server + database + user + ending;
+        this.connectionUrl = serverPrefix + server + database + user +
+                             connProperties;
+    }
+    //--------------------------------Connect---------------------------------
+    public void connect() {
         try {
             this.connection = DriverManager.getConnection(this.connectionUrl);
         } catch (SQLException ex) {
@@ -56,6 +82,7 @@ public class CoreDatabase {
             System.out.println("Failed to connect to database!");
         }
     }
+    //-------------------------------Reconnect--------------------------------
     public void reconnect() {
         try {
             this.connection = DriverManager.getConnection(this.connectionUrl);
@@ -73,6 +100,9 @@ public class CoreDatabase {
             this.reconnect();
         }
     }
+
+
+
     //--------------------------Getters-And-Setters---------------------------
     public Connection getConnection() {
         return this.connection;
