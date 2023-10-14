4591f433afb88111bd0d99385b767ac2ddb3169a
theoliverlear <sigwartholiver2@gmail.com>
2023-10-14 15:18:04 -0500

    Add matches data

--- DotaDataHub/src/main/java/org/d2database/V2/Player/PlayerDatabase.java
+++ DotaDataHub/src/main/java/org/d2database/V2/Player/PlayerDatabase.java
@@ -12,23 +12,28 @@ public class PlayerDatabase extends CoreDatabase {
     }
 
     /*
-      --PlayerV1 Columns with Metadata Sorted Relevantly
-      -- account_id (INT, unique, NOT NULL, primary key)
-      -- persona_name (VARCHAR(255), DEFAULT NULL)
-      -- name (VARCHAR(255) DEFAULT NULL)
-      -- steam_id (BIGINT DEFAULT NULL)
-      -- profile_url (VARCHAR(255) DEFAULT NULL)
-      -- avatar (VARCHAR(255) DEFAULT NULL)
-      -- loc_country_code (VARCHAR(255) DEFAULT NULL)
-      -- last_login (DATETIME DEFAULT NULL)
-      -- dota_plus (VARCHAR(4,5) DEFAULT NULL)
-      -- is_subscriber (VARCHAR(4,5) DEFAULT NULL)
-      -- is_contributor (VARCHAR(4,5) DEFAULT NULL)
-      -- mmr_estimate (INT DEFAULT NULL)
-      -- rank_tier (INT DEFAULT NULL)
-      -- solo_competitive_rank (INT DEFAULT NULL)
-      -- competitive_rank (INT DEFAULT NULL)
-      -- leaderboard_rank (INT DEFAULT NULL)
+        --PlayerV1 Columns with Metadata Sorted Relevantly
+        -- account_id (INT, unique, NOT NULL, primary key)
+        -- persona_name (VARCHAR(255), DEFAULT NULL)
+        -- name (VARCHAR(255) DEFAULT NULL)
+        -- steam_id (BIGINT DEFAULT NULL)
+        -- profile_url (VARCHAR(255) DEFAULT NULL)
+        -- avatar (VARCHAR(255) DEFAULT NULL)
+        -- loc_country_code (VARCHAR(255) DEFAULT NULL)
+        -- last_login (DATETIME DEFAULT NULL)
+        -- dota_plus (VARCHAR(4,5) DEFAULT NULL)
+        -- is_subscriber (VARCHAR(4,5) DEFAULT NULL)
+        -- is_contributor (VARCHAR(4,5) DEFAULT NULL)
+        -- mmr_estimate (INT DEFAULT NULL)
+        -- rank_tier (INT DEFAULT NULL)
+        -- solo_competitive_rank (INT DEFAULT NULL)
+        -- competitive_rank (INT DEFAULT NULL)
+        -- leaderboard_rank (INT DEFAULT NULL)
+        -- win (INT DEFAULT NULL)
+        -- loss (INT DEFAULT NULL)
+        -- total_matches (INT DEFAULT NULL)
+        -- win_rate (DECIMAL(5,2) DEFAULT NULL)
+        -- last_updated (DATETIME NOT NULL DEFAULT SWITCHOFFSET(GETDATE(), '-05:00'))
     */
     //-----------------------------Contains-Player----------------------------
     public boolean containsPlayer(Player player) {
@@ -66,33 +71,40 @@ public class PlayerDatabase extends CoreDatabase {
     public void insertPlayer(Player player) {
         String accountId = player.getAccountId();
         if (!this.containsPlayer(player)) {
-            String personaName = player.getPlayerData().getPersonaName();
-            String name = player.getPlayerData().getName();
-            String steamId = player.getPlayerData().getSteamId();
-            String profileUrl = player.getPlayerData().getProfileUrl();
-            String avatar = player.getPlayerData().getAvatar();
-            String locCountryCode = player.getPlayerData().getLocCountryCode();
-            String lastLogin = player.getPlayerData().getLastLogin();
-            String dotaPlus = player.getPlayerData().getDotaPlus();
-            String isSubscriber = player.getPlayerData().getIsSubscriber();
-            String isContributor = player.getPlayerData().getIsContributor();
-            String mmrEstimate = player.getPlayerData().getMmrEstimate();
-            String rankTier = player.getPlayerData().getRankTier();
-            String soloCompetitiveRank = player.getPlayerData().getSoloCompetitiveRank();
-            String competitiveRank = player.getPlayerData().getCompetitiveRank();
-            String leaderboardRank = player.getPlayerData().getLeaderboardRank();
+            String personaName = player.getPersonaName();
+            String name = player.getName();
+            String steamId = player.getSteamId();
+            String profileUrl = player.getProfileUrl();
+            String avatar = player.getAvatar();
+            String locCountryCode = player.getLocCountryCode();
+            String lastLogin = player.getLastLogin();
+            String dotaPlus = player.getDotaPlus();
+            String isSubscriber = player.getIsSubscriber();
+            String isContributor = player.getIsContributor();
+            String mmrEstimate = player.getMmrEstimate();
+            String rankTier = player.getRankTier();
+            String soloCompetitiveRank = player.getSoloCompetitiveRank();
+            String competitiveRank = player.getCompetitiveRank();
+            String leaderboardRank = player.getLeaderboardRank();
+            String win = player.getWin();
+            String loss = player.getLoss();
+            String totalMatches = player.getTotalMatches();
+            String winRate = player.getWinRate();
 
             String query = String.format("INSERT INTO PlayerV1 (account_id," +
-                            " persona_name, name, steam_id, profile_url, avatar," +
-                            " loc_country_code, last_login, dota_plus, " +
-                            "is_subscriber, is_contributor, mmr_estimate, rank_tier, " +
-                            "solo_competitive_rank, competitive_rank, leaderboard_rank) " +
-                            "VALUES (%s, '%s', '%s', %s, '%s', '%s', '%s', '%s'," +
-                            " '%s', '%s', '%s', %s, %s, %s, %s, %s)",
-                    accountId, personaName, name, steamId, profileUrl,
-                    avatar, locCountryCode, lastLogin, dotaPlus, isSubscriber,
+                            " persona_name, name, steam_id, profile_url," +
+                            " avatar, loc_country_code, last_login, " +
+                            "dota_plus, is_subscriber, is_contributor, " +
+                            "mmr_estimate, rank_tier, solo_competitive_rank," +
+                            " competitive_rank, leaderboard_rank, win, loss," +
+                            " total_matches, win_rate) VALUES (%s, '%s', " +
+                            "'%s', %s, '%s', '%s', '%s', '%s', '%s', '%s', " +
+                            "'%s', %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    accountId, personaName, name, steamId, profileUrl, avatar,
+                    locCountryCode, lastLogin, dotaPlus, isSubscriber,
                     isContributor, mmrEstimate, rankTier, soloCompetitiveRank,
-                    competitiveRank, leaderboardRank);
+                    competitiveRank, leaderboardRank, win, loss, totalMatches,
+                    winRate);
             this.commandQuery(query);
         }
     }
@@ -101,33 +113,42 @@ public class PlayerDatabase extends CoreDatabase {
         boolean playerDataMatches;
         String accountId = player.getAccountId();
         if (this.containsPlayer(player)) {
-            String personaName = player.getPlayerData().getPersonaName();
-            String name = player.getPlayerData().getName();
-            String steamId = player.getPlayerData().getSteamId();
-            String profileUrl = player.getPlayerData().getProfileUrl();
-            String avatar = player.getPlayerData().getAvatar();
-            String locCountryCode = player.getPlayerData().getLocCountryCode();
-            String lastLogin = player.getPlayerData().getLastLogin();
-            String dotaPlus = player.getPlayerData().getDotaPlus();
-            String isSubscriber = player.getPlayerData().getIsSubscriber();
-            String isContributor = player.getPlayerData().getIsContributor();
-            String mmrEstimate = player.getPlayerData().getMmrEstimate();
-            String rankTier = player.getPlayerData().getRankTier();
-            String soloCompetitiveRank = player.getPlayerData().getSoloCompetitiveRank();
-            String competitiveRank = player.getPlayerData().getCompetitiveRank();
-            String leaderboardRank = player.getPlayerData().getLeaderboardRank();
+            String personaName = player.getPersonaName();
+            String name = player.getName();
+            String steamId = player.getSteamId();
+            String profileUrl = player.getProfileUrl();
+            String avatar = player.getAvatar();
+            String locCountryCode = player.getLocCountryCode();
+            String lastLogin = player.getLastLogin();
+            String dotaPlus = player.getDotaPlus();
+            String isSubscriber = player.getIsSubscriber();
+            String isContributor = player.getIsContributor();
+            String mmrEstimate = player.getMmrEstimate();
+            String rankTier = player.getRankTier();
+            String soloCompetitiveRank = player.getSoloCompetitiveRank();
+            String competitiveRank = player.getCompetitiveRank();
+            String leaderboardRank = player.getLeaderboardRank();
+            String win = player.getWin();
+            String loss = player.getLoss();
+            String totalMatches = player.getTotalMatches();
+            String winRate = player.getWinRate();
 
-            String query = String.format("SELECT * FROM PlayerV1 WHERE account_id = %s" +
-                            " AND persona_name = '%s' AND name = '%s' AND steam_id = %s" +
-                            " AND profile_url = '%s' AND avatar = '%s' AND loc_country_code = '%s'" +
-                            " AND last_login = '%s' AND dota_plus = '%s' AND is_subscriber = '%s'" +
-                            " AND is_contributor = '%s' AND mmr_estimate = %s AND rank_tier = %s" +
-                            " AND solo_competitive_rank = %s AND competitive_rank = %s" +
-                            " AND leaderboard_rank = %s",
+            String query = String.format("SELECT * FROM PlayerV1 WHERE " +
+                            "account_id = %s AND persona_name = '%s' AND " +
+                            "name = '%s' AND steam_id = %s AND profile_url = '%s' " +
+                            "AND avatar = '%s' AND loc_country_code = '%s' AND " +
+                            "last_login = '%s' AND dota_plus = '%s' AND " +
+                            "is_subscriber = '%s' AND is_contributor = '%s' AND " +
+                            "mmr_estimate = %s AND rank_tier = %s AND " +
+                            "solo_competitive_rank = %s AND competitive_rank = %s " +
+                            "AND leaderboard_rank = %s AND win = %s AND loss = %s " +
+                            "AND total_matches = %s AND win_rate = %s",
                     accountId, personaName, name, steamId, profileUrl, avatar,
-                    locCountryCode, lastLogin, dotaPlus, isSubscriber, isContributor,
-                    mmrEstimate, rankTier, soloCompetitiveRank, competitiveRank,
-                    leaderboardRank);
+                    locCountryCode, lastLogin, dotaPlus, isSubscriber,
+                    isContributor, mmrEstimate, rankTier, soloCompetitiveRank,
+                    competitiveRank, leaderboardRank, win, loss, totalMatches,
+                    winRate);
+            this.commandQuery(query);
             try {
                 ResultSet resultSet;
                 try (Statement statement = this.getConnection().createStatement()) {
@@ -152,32 +173,39 @@ public class PlayerDatabase extends CoreDatabase {
     public void updatePlayer(Player player) {
         String accountId = player.getAccountId();
         if (this.containsPlayer(player)) {
-            String personaName = player.getPlayerData().getPersonaName();
-            String name = player.getPlayerData().getName();
-            String steamId = player.getPlayerData().getSteamId();
-            String profileUrl = player.getPlayerData().getProfileUrl();
-            String avatar = player.getPlayerData().getAvatar();
-            String locCountryCode = player.getPlayerData().getLocCountryCode();
-            String lastLogin = player.getPlayerData().getLastLogin();
-            String dotaPlus = player.getPlayerData().getDotaPlus();
-            String isSubscriber = player.getPlayerData().getIsSubscriber();
-            String isContributor = player.getPlayerData().getIsContributor();
-            String mmrEstimate = player.getPlayerData().getMmrEstimate();
-            String rankTier = player.getPlayerData().getRankTier();
-            String soloCompetitiveRank = player.getPlayerData().getSoloCompetitiveRank();
-            String competitiveRank = player.getPlayerData().getCompetitiveRank();
-            String leaderboardRank = player.getPlayerData().getLeaderboardRank();
+            String personaName = player.getPersonaName();
+            String name = player.getName();
+            String steamId = player.getSteamId();
+            String profileUrl = player.getProfileUrl();
+            String avatar = player.getAvatar();
+            String locCountryCode = player.getLocCountryCode();
+            String lastLogin = player.getLastLogin();
+            String dotaPlus = player.getDotaPlus();
+            String isSubscriber = player.getIsSubscriber();
+            String isContributor = player.getIsContributor();
+            String mmrEstimate = player.getMmrEstimate();
+            String rankTier = player.getRankTier();
+            String soloCompetitiveRank = player.getSoloCompetitiveRank();
+            String competitiveRank = player.getCompetitiveRank();
+            String leaderboardRank = player.getLeaderboardRank();
+            String win = player.getWin();
+            String loss = player.getLoss();
+            String totalMatches = player.getTotalMatches();
+            String winRate = player.getWinRate();
 
-            String query = String.format("UPDATE PlayerV1 SET persona_name = '%s'," +
-                            " name = '%s', steam_id = %s, profile_url = '%s', avatar = '%s'," +
-                            " loc_country_code = '%s', last_login = '%s', dota_plus = '%s', " +
-                            "is_subscriber = '%s', is_contributor = '%s', mmr_estimate = %s, " +
-                            "rank_tier = %s, solo_competitive_rank = %s, competitive_rank = %s, " +
-                            "leaderboard_rank = %s WHERE account_id = %s",
-                    personaName, name, steamId, profileUrl, avatar, locCountryCode,
-                    lastLogin, dotaPlus, isSubscriber, isContributor, mmrEstimate,
-                    rankTier, soloCompetitiveRank, competitiveRank, leaderboardRank,
-                    accountId);
+            String query = String.format("UPDATE PlayerV1 SET persona_name = '%s', " +
+                            "name = '%s', steam_id = %s, profile_url = '%s', " +
+                            "avatar = '%s', loc_country_code = '%s', last_login = '%s', " +
+                            "dota_plus = '%s', is_subscriber = '%s', is_contributor = '%s', " +
+                            "mmr_estimate = %s, rank_tier = %s, solo_competitive_rank = %s, " +
+                            "competitive_rank = %s, leaderboard_rank = %s, win = %s, " +
+                            "loss = %s, total_matches = %s, win_rate = %s WHERE " +
+                            "account_id = %s",
+                    personaName, name, steamId, profileUrl, avatar,
+                    locCountryCode, lastLogin, dotaPlus, isSubscriber,
+                    isContributor, mmrEstimate, rankTier, soloCompetitiveRank,
+                    competitiveRank, leaderboardRank, win, loss, totalMatches,
+                    winRate, accountId);
             this.commandQuery(query);
         } else {
             System.out.println("Player (" + accountId + ") does not exist in " +
