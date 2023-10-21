04c096299adfdd8b16e229ebc822c887cedb1622
theoliverlear <sigwartholiver2@gmail.com>
2023-10-21 13:49:10 -0500

    Add small matches to the database

--- DotaDataHub/src/main/java/org/d2database/V2/Hub/PlayerHub.java
+++ DotaDataHub/src/main/java/org/d2database/V2/Hub/PlayerHub.java
@@ -3,6 +3,8 @@ package org.d2database.V2.Hub;
 import org.d2database.V2.Data;
 import org.d2database.V2.FileDataRetriever;
 import org.d2database.V2.Player.Player;
+import org.d2database.V2.Player.SmallPlayerMatch;
+import org.d2database.V2.Player.SmallPlayerMatchDatabase;
 
 public class PlayerHub {
     String accountId;
@@ -10,6 +12,7 @@ public class PlayerHub {
     public PlayerHub(String accountId) {
         this.accountId = accountId;
         this.player = new Player(this.accountId);
+        System.out.println("Starting a Player Hub for player: " + this.accountId);
     }
     public static void main(String[] args) {
         //-------------------------Set-API-Variable---------------------------
@@ -19,22 +22,40 @@ public class PlayerHub {
         String apiKey = fileDataRetriever.getData();
         Data.API_KEY.setData(apiKey);
         //-----------------------Connection-Variables-------------------------
-        PlayerHub playerHub = new PlayerHub("348245720");
-//        PlayerDataHub playerHub2 = new PlayerDataHub("192960022");
-//        PlayerDataHub playerHub3 = new PlayerDataHub("126867101");
+        PlayerHub playerHubOne = new PlayerHub("348245720");
+        PlayerHub playerHubTwo = new PlayerHub("192960022");
+        PlayerHub playerHubThree = new PlayerHub("126867101");
         //-------------------------Print-Statements---------------------------
-//        PlayerDatabase playerDB = new PlayerDatabase();
-//        System.out.println("Contains player: " + playerDB.containsPlayer(playerHub.getPlayer()));
-//        playerDB.insertSequence(playerHub.getPlayer());
-//
-//        System.out.println("Contains player: " + playerDB.containsPlayer(playerHub2.getPlayer()));
-//        playerDB.insertSequence(playerHub2.getPlayer());
-//
-//        System.out.println("Contains player: " + playerDB.containsPlayer(playerHub3.getPlayer()));
-//        playerDB.insertSequence(playerHub3.getPlayer());
-
         // Like how the matches for each player is a web crawler for the matches,
         // the players each are a web crawler for other players.
+
+        SmallPlayerMatchDatabase smallPlayerMatchDatabase = new SmallPlayerMatchDatabase();
+
+        Thread threadOne = new Thread(() -> {
+            for (SmallPlayerMatch match : playerHubOne.getPlayer().getPlayerMatches()) {
+                smallPlayerMatchDatabase.insertSequence(match);
+            }
+        });
+        Thread threadTwo = new Thread(() -> {
+            for (SmallPlayerMatch match : playerHubTwo.getPlayer().getPlayerMatches()) {
+                smallPlayerMatchDatabase.insertSequence(match);
+            }
+        });
+        Thread threadThree = new Thread(() -> {
+            for (SmallPlayerMatch match : playerHubThree.getPlayer().getPlayerMatches()) {
+                smallPlayerMatchDatabase.insertSequence(match);
+            }
+        });
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+        try {
+            threadOne.join();
+            threadTwo.join();
+            threadThree.join();
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
     }
     //-------------------------------Getters----------------------------------
     public String getAccountId() {
