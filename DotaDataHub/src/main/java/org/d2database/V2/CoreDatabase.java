3e7274cfdb5c6eee32fedc77b5ac25aa31036633
theoliverlear <sigwartholiver2@gmail.com>
2023-10-21 13:46:18 -0500

    Add method for query results

--- DotaDataHub/src/main/java/org/d2database/V2/CoreDatabase.java
+++ DotaDataHub/src/main/java/org/d2database/V2/CoreDatabase.java
@@ -1,8 +1,5 @@
 package org.d2database.V2;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 public class CoreDatabase {
@@ -72,13 +69,42 @@ public class CoreDatabase {
     //---------------------------Make-Query-Request---------------------------
     public void commandQuery(String command) {
         try {
-            try (PreparedStatement statement = this.getConnection().prepareStatement(command)) {
+            try (PreparedStatement statement = this.connection.prepareStatement(command)) {
                 statement.execute();
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
             this.reconnect();
         }
+    }
+    //--------------------------Make-Result-Request---------------------------
+    public ResultSet resultQuery(String command) {
+        try {
+            try (PreparedStatement statement = this.connection.prepareStatement(command)) {
+                statement.execute();
+                ResultSet resultSet = statement.getResultSet();
+                return resultSet;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            this.reconnect();
+        }
+        return null;
+    }
+    //---------------------------Has-Result-Request---------------------------
+    public boolean queryHasResult(String query) {
+        boolean queryHasResult = true; // Default to true to prevent duplicate
+                                       // matches from being inserted
+        try {
+            try (PreparedStatement statement = this.connection.prepareStatement(query)){
+                statement.execute();
+                ResultSet resultSet = statement.getResultSet();
+                queryHasResult = resultSet.next();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return queryHasResult;
     }
     //--------------------------Getters-And-Setters---------------------------
     public Connection getConnection() {
