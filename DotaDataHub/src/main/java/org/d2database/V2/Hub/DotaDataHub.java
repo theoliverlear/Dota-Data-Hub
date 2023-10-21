3125c6d60073669d5ecfe8991ac9f7986fac2d96
theoliverlear <sigwartholiver2@gmail.com>
2023-10-21 13:47:22 -0500

    Add TODO comment

--- DotaDataHub/src/main/java/org/d2database/V2/Hub/DotaDataHub.java
+++ DotaDataHub/src/main/java/org/d2database/V2/Hub/DotaDataHub.java
@@ -1,8 +1,13 @@
 package org.d2database.V2.Hub;
 
-public class DotaDataHub {
+import org.d2database.V2.Player.Player;
+import org.d2database.V2.Player.SmallPlayerMatchDatabase;
 
-    public DotaDataHub(String accountId) {
+public class DotaDataHub {
+    // TODO: perhaps there should be constructors for handling all the
+    //       different types of data needed to make requests to the OpenDota
+    //       API
+    public DotaDataHub(Player player) {
 
     }
     public static void main(String[] args) {
