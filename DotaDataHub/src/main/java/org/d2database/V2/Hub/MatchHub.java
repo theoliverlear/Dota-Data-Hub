b26f6d37bf4119548bdd0109b80c2db1b10a5fc5
theoliverlear <sigwartholiver2@gmail.com>
2023-10-21 19:51:48 -0500

    Start class functionality

--- DotaDataHub/src/main/java/org/d2database/V2/Hub/MatchHub.java
+++ DotaDataHub/src/main/java/org/d2database/V2/Hub/MatchHub.java
@@ -1,7 +1,29 @@
 package org.d2database.V2.Hub;
 
+import org.d2database.V2.Match.Match;
+
 public class MatchHub {
+    String matchId;
+    Match match;
+    public MatchHub(String matchId) {
+        this.matchId = matchId;
+        this.match = new Match(this.matchId);
+    }
     public static void main(String[] args) {
 
+    }
+    //-------------------------------Getters----------------------------------
+    public String getMatchId() {
+        return this.matchId;
+    }
+    public Match getMatch() {
+        return this.match;
+    }
+    //-------------------------------Setters----------------------------------
+    public void setMatchId(String matchId) {
+        this.matchId = matchId;
+    }
+    public void setMatch(Match match) {
+        this.match = match;
     }
 }
