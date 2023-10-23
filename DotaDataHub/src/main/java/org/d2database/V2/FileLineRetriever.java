d82883b0f3ab3ce7f0d81a205d81b1eddda055ab
theoliverlear <sigwartholiver2@gmail.com>
2023-10-23 08:29:56 -0500

    Rename file

--- DotaDataHub/src/main/java/org/d2database/V2/FileLineRetriever.java
+++ DotaDataHub/src/main/java/org/d2database/V2/FileLineRetriever.java
@@ -3,32 +3,44 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class FileDataRetriever {
+public class FileLineRetriever {
     int targetLineIndex;
     String filePath;
     String data;
-    public FileDataRetriever(int targetLineIndex, String filePath) {
+    /**
+     * This constructor takes a target line index and a file path as arguments.
+     * @param targetLineIndex
+     * @param filePath
+     */
+    public FileLineRetriever(int targetLineIndex, String filePath) {
         this.targetLineIndex = targetLineIndex;
         this.filePath = filePath;
         this.data = "";
         this.fetchData();
     }
+    /**
+     * This method retrieves line data from a file.
+     */
     public void fetchData() {
         String line = "";
         try {
-            int counter = 0;
+            int lineCounter = 0;
             Scanner fileReader = new Scanner(new File(this.filePath));
             while (fileReader.hasNext()) {
                 line = fileReader.nextLine();
-                if (counter == this.targetLineIndex) {
+                if (lineCounter == this.targetLineIndex) {
                     this.data = line;
                 }
-                counter++;
+                lineCounter++;
             }
-        } catch (FileNotFoundException err) {
-            err.printStackTrace();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
         }
     }
+    /**
+     * This method returns the data retrieved from the file.
+     * @return String
+     */
     public String getData() {
         return this.data;
     }
