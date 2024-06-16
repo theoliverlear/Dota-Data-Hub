bdd69d372cae06fa116c05306ca81f61763a01d9
theoliverlear <sigwartholiver2@gmail.com>
2024-06-16 17:28:25 -0500

    Add Player.java in org.theoliverlear.entity.player package to represent a given player

---
+++ src/main/java/org/theoliverlear/entity/player/Player.java
@@ -0,0 +1,5 @@
+package org.theoliverlear.entity.player;
+// TODO: When a new user is created, all their data goes into the database.
+//       This is a temporary operation for data during the development phase.
+public class Player {
+}
