88359e32f2780a1755db51414bdef6e187ea1276
theoliverlear <sigwartholiver2@gmail.com>
2024-06-16 17:18:18 -0500

    Add DotaDataHubApplication.java as the main application entry point for the Dota Data Hub project

---
+++ src/main/java/org/theoliverlear/DotaDataHubApplication.java
@@ -0,0 +1,11 @@
+package org.theoliverlear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaDataHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DotaDataHubApplication.class, args);
+    }
+}
