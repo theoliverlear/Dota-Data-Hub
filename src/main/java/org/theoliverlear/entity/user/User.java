b25ae4e2b84c4a89cf3a7ecc6101d19a931aa69c
theoliverlear <sigwartholiver2@gmail.com>
2024-06-16 17:14:44 -0500

    Add User.java to represent a given user of Dota Data Hub

---
+++ src/main/java/org/theoliverlear/entity/user/User.java
@@ -0,0 +1,16 @@
+package org.theoliverlear.entity.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Embedded
+    private SafePassword safePassword;
+
+}
