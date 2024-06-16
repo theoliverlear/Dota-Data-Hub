fae649c3d3c10649b23254697b95258267e3fe80
theoliverlear <sigwartholiver2@gmail.com>
2024-06-16 16:59:35 -0500

    Add SecurityConfig.java with security filter chain and password encoder configurations

---
+++ src/main/java/org/theoliverlear/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.theoliverlear.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    //==============================-Beans-===================================
+
+    //------------------------Security-Filter-Chain---------------------------
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // HttpSecurity is the builder object used to create the security
+        // filter chain. We authorize all requests and disable CSRF.
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf()
+                .disable();
+        return http.build();
+    }
+    //--------------------------Password-Encoder------------------------------
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
